f16f01f93baffbbd39a8b5dd3a9d8bd0ac096b7a
Irina <iram4@yandex.ru>
2018-12-03 23:34:53 +0300

    Урок 9

---
+++ project2/lesson_9/js/script.js
@@ -0,0 +1,109 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+    //Timer 
+
+    let deadLine = '2018-11-31';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+            if (seconds < 10) seconds = '0' + seconds;
+            if (minutes < 10) minutes = '0' + minutes;
+            if (hours < 10) hours = '0' + hours;  
+        return {
+            'total' : t,
+            "hours" : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+          
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                document.querySelector('.seconds').innerHTML = '00';
+                document.querySelector('.minutes').innerHTML = '00';
+                document.querySelector('.hours').innerHTML = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadLine);
+
+    // Modal
+
+    let container = document.querySelector('body'),
+        more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        descriptionBtn = document.querySelector('.description-btn');
+        
+    container.addEventListener('click', function(e) {
+        if(
+            e.target && e.target.classList('more') || e.target.classList('description-btn')) {
+            overlay.style.display = 'block';
+            more.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        }
+    });
+
+    // more.addEventListener('click', function() {
+    //     overlay.style.display = 'block';
+    //     this.classList.add('more-splash');
+    //     document.body.style.overflow = 'hidden';
+    // });
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+});
+
